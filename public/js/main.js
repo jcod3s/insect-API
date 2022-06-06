cac18d4b274255c5ca3042ff38e95818c1d969b1
Jose Perez <jcodes.knosyn@gmail.com>
2022-06-06 13:17:19 -0400

    fetching insect data

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 const getData = document.querySelector('#getData');
-const insect = document.querySelector('#insect')
+const insectName = document.querySelector('#insect')
 
-getData.addEventListener('click', function() {
-    fetch(`https://murmuring-forest-20754.herokuapp.com/api/${insect}`)
+getData.addEventListener('click', async function() {
+    console.log('fetching data')
+    const getData = await fetch(`https://murmuring-forest-20754.herokuapp.com/api/${insect}`)
+    const insects = await res.json()
+    console.log(insects[insectName])
 })
