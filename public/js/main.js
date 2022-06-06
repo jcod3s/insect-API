fd496fcc0571849f579db1f71650a574533583c6
Jose Perez <jcodes.knosyn@gmail.com>
2022-06-06 12:31:33 -0400

    fixed fetch request

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ const getData = document.querySelector('#getData');
 const insect = document.querySelector('#insect')
 
 getData.addEventListener('click', function() {
-    fetch(`localhost:8000/api?=${insect}`)
+    fetch(`https://murmuring-forest-20754.herokuapp.com/api?=${insect}`)
 })
