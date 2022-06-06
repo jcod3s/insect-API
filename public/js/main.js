ff47c5e9c89cc1a8ea1590ee26b35d20069b2c06
Jose Perez <jcodes.knosyn@gmail.com>
2022-06-06 13:07:28 -0400

    changed fetch request

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ const getData = document.querySelector('#getData');
 const insect = document.querySelector('#insect')
 
 getData.addEventListener('click', function() {
-    fetch(`https://murmuring-forest-20754.herokuapp.com/api?=${insect}`)
+    fetch(`https://murmuring-forest-20754.herokuapp.com/api/${insect}`)
 })
