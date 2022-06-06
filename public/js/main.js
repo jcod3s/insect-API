44b19816f87b30177e42ae6d514a958df50d83c8
Jose Perez <jcodes.knosyn@gmail.com>
2022-06-06 14:58:22 -0400

    fixing continuous text add issue and making pre-query state clean

--- public/js/main.js
+++ public/js/main.js
@@ -11,11 +11,12 @@ getData.addEventListener('click', function() {
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         const insectData = data
-        document.querySelector('#sciName').textContent += insectData.scientificName
-        document.querySelector('#order').textContent += insectData.order
-        document.querySelector('#lifeSpan').textContent += insectData.lifeSpan
-        document.querySelector('#climates').textContent += insectData.climates
-        document.querySelector('#description').textContent += insectData.description
+        document.querySelector('#sciName').textContent = 'Scientific Name: '+ insectData.scientificName
+        document.querySelector('#order').textContent = 'Order: '+ insectData.order
+        document.querySelector('#lifeSpan').textContent = 'Average Life Span: ' + insectData.lifeSpan
+        document.querySelector('#climates').textContent = 'Typical Climates: ' + insectData.climates
+        document.querySelector('#descHeader').textContent = 'Description'
+        document.querySelector('#description').textContent = insectData.description
       })
       .then()
       .catch(err => {
