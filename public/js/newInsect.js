61a31fccccf4ec734d244f9d8eb364f00f4c5e24
Jose Perez <98285717+jcod3s@users.noreply.github.com>
2022-06-22 07:19:02 -0400

    Delete public directory

--- public/js/newInsect.js
+++
@@ -1,7 +0,0 @@
-const newInsect = document.querySelector('#newInsectNav');
-
-newInsect.addEventListener('click', function(){
-    const url = `https://insect-api.herokuapp.com/api/addInsect`
-
-    fetch(url)
-})
