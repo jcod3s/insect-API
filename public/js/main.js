66431364d475be395c0f89a1a2a3748300f7d74e
Jose Perez <jcodes.knosyn@gmail.com>
2022-06-30 13:24:23 -0400

    testing whether server side js can handle the whole process of res.rendering

--- public/js/main.js
+++ public/js/main.js
@@ -14,16 +14,16 @@ getData.addEventListener('click', function() {
   const url = `https://insect-api.herokuapp.com/api/${insect}`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        const insectData = data
-        document.querySelector('#sciName').textContent = 'Scientific Name: '+ insectData.scientificName
-        document.querySelector('#order').textContent = 'Order: '+ insectData.order
-        document.querySelector('#lifeSpan').textContent = 'Average Life Span: ' + insectData.lifeSpan
-        document.querySelector('#climates').textContent = 'Typical Climates: ' + insectData.climates
-        document.querySelector('#descHeader').textContent = 'Description'
-        document.querySelector('#description').textContent = insectData.description
-      })
+      // .then(res => res.json()) // parse response as JSON
+      // .then(data => {
+      //   const insectData = data
+      //   document.querySelector('#sciName').textContent = 'Scientific Name: '+ insectData.scientificName
+      //   document.querySelector('#order').textContent = 'Order: '+ insectData.order
+      //   document.querySelector('#lifeSpan').textContent = 'Average Life Span: ' + insectData.lifeSpan
+      //   document.querySelector('#climates').textContent = 'Typical Climates: ' + insectData.climates
+      //   document.querySelector('#descHeader').textContent = 'Description'
+      //   document.querySelector('#description').textContent = insectData.description
+      // })
       .catch(err => {
           console.log(`error ${err}`)
       });
