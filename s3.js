709d5cd72725ffadf402b1111a0969443fa2dafe
jcod3s <98285717+jcod3s@users.noreply.github.com>
2022-08-02 22:24:02 -0400

    removed cerds

--- s3.js
+++ s3.js
@@ -4,17 +4,17 @@ const fs = require('fs')
 
 DB_STRING = 'mongodb+srv://pdiddy:sdiddyCombs@insectcluster.hx7ipsq.mongodb.net/?retryWrites=true&w=majority'
 
-const awsBucketName = "insectimages"
-const awsBucketRegion = "us-east-1"
-const awsAccessKEy = "AKIA5BW2VVPUSSCXWHOE"
-const awsSecretKey = "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"
+const awsBucketName = process.env.awsBucketName
+const awsBucketRegion = process.env.awsBucketRegion
+const awsAccessKey = process.env.awsAccessKey
+const awsSecretKey = process.env.awsSecretKey
 
 
 
 
 const s3 = new S3({
     awsBucketRegion,
-    awsAccessKEy,
+    awsAccessKey,
     awsSecretKey,
 })
 
